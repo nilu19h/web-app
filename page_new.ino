11b68db7230a8740c6c5b93cd0aa3d78156711b0
nilu19h <163760793+nilu19h@users.noreply.github.com>
2024-04-05 17:54:20 +0530

    Update page_new.ino

--- page_new.ino
+++ page_new.ino
@@ -2,4 +2,22 @@
 #include <Wi-Fi>
 
 // network credentials
-const char* s  
+const char* ssid = "ESP32-NETWORK";
+const char* password = "ESP32AFNAN";
+
+// server listening to port 80
+WiFiServer server(80)
+
+// HTTP request
+string header;
+
+// todo
+
+void setup() {
+  Serial.begin(115200);
+
+  // todo 
+
+  WiFi.softAP(ssid,password);
+}
+
